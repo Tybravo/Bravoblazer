d0824547b42db9200c073733cd75ebe9e6ec0178
Adetayo <twinebravo@gmail.com>
2024-06-20 14:26:51 +0100

    Added java file

---
+++ DisplayShapesWithAsterisks.java
@@ -0,0 +1,36 @@
+//Question 2.18
+
+
+public class DisplayShapesWithAsterisks {
+
+	public static void main(String[] args){
+	
+		
+	char isAsterisks = '*';
+	String isEmpty = " ";
+	
+
+	System.out.printf("%c %c %c %c %c %c %c %c %c %s %s %s %s %s %c %c %c %s %s %s %s %s %s %s %c %s %s %s %s %s %s %s %s %c %s %s %s %s %n", isAsterisks, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isAsterisks, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty);
+	
+	System.out.printf("%c %s %s %s %s %s %s %s %c %s %s %s %c %s %s %s %s %s %c %s %s %s %s %c %c %c %s %s %s %s %s %s %c %s %c %s %s %s %n", isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isAsterisks, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty);
+
+	System.out.printf("%c %s %s %s %s %s %s %s %c %s %s %c %s %s %s %s %s %s %s %c %s %s %c %c %c %c %c %s %s %s %s %c %s %s %s %c %s %s %n", isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty);
+
+	System.out.printf("%c %s %s %s %s %s %s %s %c %s %s %c %s %s %s %s %s %s %s %c %s %s %s %s %c %s %s %s %s %s %c %s %s %s %s %s %c %s %n", isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty);
+
+	System.out.printf("%c %s %s %s %s %s %s %s %c %s %s %c %s %s %s %s %s %s %s %c %s %s %s %s %c %s %s %s %s %c %s %s %s %s %s %s %s %c %n", isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks);
+
+	System.out.printf("%c %s %s %s %s %s %s %s %c %s %s %c %s %s %s %s %s %s %s %c %s %s %s %s %c %s %s %s %s %s %c %s %s %s %s %s %c %s %n", isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty);
+
+	System.out.printf("%c %s %s %s %s %s %s %s %c %s %s %c %s %s %s %s %s %s %s %c %s %s %s %s %c %s %s %s %s %s %s %c %s %s %s %c %s %s %n", isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty);
+
+
+	System.out.printf("%c %s %s %s %s %s %s %s %c %s %s %s %c %s %s %s %s %s %c %s %s %s %s %s %c %s %s %s %s %s %s %s %c %s %c %s %s %s %n", isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty);
+
+	System.out.printf("%c %c %c %c %c %c %c %c %c %s %s %s %s %s %c %c %c %s %s %s %s %s %s %s %c %s %s %s %s %s %s %s %s %c %s %s %s %s %n", isAsterisks, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isAsterisks, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isEmpty, isAsterisks, isEmpty, isEmpty, isEmpty, isEmpty);
+	
+
+    }
+
+}
+
