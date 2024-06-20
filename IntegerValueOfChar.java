dd0a0a1c75927ad189a23807b9b1d6995cec1f4f
Adetayo <twinebravo@gmail.com>
2024-06-20 14:33:27 +0100

    Added java file

---
+++ IntegerValueOfChar.java
@@ -0,0 +1,42 @@
+//Question 2.29
+
+
+public class IntegerValueOfChar {
+
+	public static void main(String[] args){
+
+	int isUpperA = 'A';
+	int isUpperB = 'B';
+	int isUpperC = 'C';
+	int isSmallA = 'a';
+	int isSmallB = 'b';
+	int isSmallC = 'c';
+	int isZero = '0';
+	int isOne = '1';
+	int isTwo = '2';
+	int isDollar = '$';
+	int isAsterisk = '*';	
+	int isPlus = '+';
+	int isForwardSlash = '/';
+	int isBlank = ' ';
+
+	System.out.printf("The character %c has the value %d%n", isUpperA, ((int) isUpperA));
+	System.out.printf("The character %c has the value %d%n", isUpperB, ((int) isUpperB));
+	System.out.printf("The character %c has the value %d%n", isUpperC, ((int) isUpperC));
+	System.out.printf("The character %c has the value %d%n", isSmallA, ((int) isSmallA));
+	System.out.printf("The character %c has the value %d%n", isSmallB, ((int) isSmallB));
+	System.out.printf("The character %c has the value %d%n", isSmallC, ((int) isSmallC));
+	System.out.printf("The character %c has the value %d%n", isZero, ((int) isZero));
+	System.out.printf("The character %c has the value %d%n", isOne, ((int) isOne));
+	System.out.printf("The character %c has the value %d%n", isTwo, ((int) isTwo));
+	System.out.printf("The character %c has the value %d%n", isDollar, ((int) isDollar));
+	System.out.printf("The character %c has the value %d%n", isAsterisk, ((int) isAsterisk));
+	System.out.printf("The character %c has the value %d%n", isPlus, ((int) isPlus));
+	System.out.printf("The character %c has the value %d%n", isForwardSlash, ((int) isForwardSlash));
+	System.out.printf("The character %c has the value %d%n", isBlank, ((int) isBlank));
+	
+
+
+    }
+
+}
